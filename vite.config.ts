7d09825ad44dc7c8ceaa97b5575787f9c8c5f349
Ethan Standel <ethan@standel.io>
2022-09-07 20:14:43 -0400

    Adds tests and cleans up repo.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-import dts from 'vite-plugin-dts';
-import path from 'path';
+import { defineConfig } from "vitest/config";
+import preact from "@preact/preset-vite";
+import dts from "vite-plugin-dts";
+import path from "path";
+
+const coverage = 100;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    coverage: {
+      statements: coverage,
+      functions: coverage,
+      branches: coverage,
+      lines: coverage
+    }
+  },
   plugins: [preact(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/store.ts'),
-      name: 'preact-signal-store',
+      entry: path.resolve(__dirname, "src/store.ts"),
+      name: "preact-signal-store",
       fileName: (format) => `lib.${format}.js`
     },
-    rollupOptions: { external: ['@preact/signals'] }
+    rollupOptions: { external: ["@preact/signals"] }
   },
 });
